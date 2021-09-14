2f216644dc2cb51890c9a5fd7ad3fb3b5a80cb65
j.mir <j.mir.prazak@gmail.com>
2021-09-14 20:03:17 +0200

    bridges

--- dgram.js
+++ dgram.js
@@ -53,7 +53,7 @@ server.on('message', (msg, rinfo) => {
 				command: "connect",
 				address: rinfo.address,
 				port: rinfo.port,
-				order: a,
+				order: "a",
 				date: date
 
 			}), register[secret_string].port, register[secret_string].address )
