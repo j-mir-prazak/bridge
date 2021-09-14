e9d1bd286efa3e7e7105f92834a848ea9aa8b90d
j.mir <j.mir.prazak@gmail.com>
2021-09-14 14:18:50 +0200

    bridges

--- dgram_client.js
+++ dgram_client.js
@@ -1,6 +1,8 @@
 var dgram = require('dgram');
 
-var client = dgram.createSocket('udp4');
+var client = dgram.createSocket({type:'udp4',reuseAddr:true});
+
+var peer = null
 
 var bind_port = process.argv[2] || false
 var remote_addr = process.argv[3] || false
@@ -18,28 +20,101 @@ client.on('message', (msg, rinfo) => {
 
   console.log(`client got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 
+  if ( peer == null ) {
+   var json = JSON.parse(msg)
+    console.log(json)
+
+
+    if ( json && json.port && json.address ) {
+      client.close()
+      // peer = true
+      // client.connect(json.port,json.address)
+      // setInterval(function(){
+      //   var message = Buffer.from('Some bytes');
+      //   client.send(message, json.port, json.address, function(){console.log("sent.")})
+      // }.bind(null,json),20)
+
+      peer = setupPeer(json.port, json.address)
+
+    }
+  }
+
+  else {
+
+    console.log("peer message")
+
+
+  }
+
 });
 
 client.on('connect', () => {
 
-	console.log("something is connecting.")
 
-  const message = Buffer.from('secret:abcedf:secret');
+  if ( peer == null ) {
+  	console.log("connecting somewhere.")
+    var message = Buffer.from('secret:abcedf:secret');
+    client.send(message)
+  }
 
-  client.send(message)
 
 })
 
 client.on('listening', () => {
 
   var address = client.address();
-
   console.log(`client listening ${address.address}:${address.port}`);
 
 });
+
+client.on('close', () => {
+
+  console.log("closed.")
+
+});
+
+
 
 
 client.bind(bind_port)
 client.connect(remote_port, remote_addr)
 
 // Prints: client listening 0.0.0.0:41234
+function setupPeer(port, address) {
+
+  var peer = dgram.createSocket({type:'udp4',reuseAddr:true});
+
+  peer.on('message', (msg, rinfo) => {
+
+    console.log(msg)
+
+
+  });
+
+  peer.on('connect', () => {
+
+  	console.log("connected as peer.")
+    // console.log(peer)
+    // var message = Buffer.from('secret:abcedf:secret');
+    setInterval(function() {
+          var message = Buffer.from('Some bytes');
+          // console.log(message)
+          peer.send(message, port, address)
+
+        }, 20)
+    // peer.send(message)
+
+  })
+
+  peer.on('listening', () => {
+
+    console.log('listening as peer.')
+
+  });
+
+
+
+  peer.bind(bind_port)
+  peer.connect(port, address)
+
+}
