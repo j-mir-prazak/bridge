79be6f8a2b0437d5d7d532e580180688b75bb02e
j.mir <j.mir.prazak@gmail.com>
2021-09-14 12:52:13 +0200

    bridges

---
+++ dgram_client.js
@@ -0,0 +1,42 @@
+var dgram = require('dgram');
+
+var client = dgram.createSocket('udp4');
+
+var bind_port = process.argv[2] || false
+var remote_addr = process.argv[3] || false
+var remote_port = process.argv[4] || false
+
+client.on('error', (err) => {
+
+  console.log(`client error:\n${err.stack}`);
+
+  client.close();
+
+});
+
+// client.on('message', (msg, rinfo) => {
+//
+//   console.log(`client got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+// 	client.send("got u!", rinfo.port, rinfo.address)
+//
+// });
+
+client.on('connect', () => {
+
+	console.log("something is connecting.")
+
+})
+
+client.on('listening', () => {
+
+  var address = client.address();
+
+  console.log(`client listening ${address.address}:${address.port}`);
+
+});
+
+
+client.bind(bind_port);
+client.connnect(remote_port, remote_addr)
+
+// Prints: client listening 0.0.0.0:41234
