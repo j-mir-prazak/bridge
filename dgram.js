0b2c68baba82db91b5bca1fde8145ec180082fb9
j.mir <j.mir.prazak@gmail.com>
2021-09-14 12:36:49 +0200

    bridges

--- dgram.js
+++ dgram.js
@@ -8,7 +8,10 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, rinfo) => {
+
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+	socket.send("got u!", rinfo.port, rinfo.address)
+
 });
 
 server.on('listening', () => {
